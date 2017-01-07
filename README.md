5b134bd616b3d2e5a782322c0d991c7d4e49dafb
Xavier <qazxiaye@126.com>
2017-01-07 23:52:09 +0100

    add unzip

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 This tool uses a batch of images as backgrounds of slides in a generated powerpoint.
 
-The images should be in the working directory and in jpg format (can modify in source). There will be no blank margin in generated powerpoint.
+The images should be in the working directory and in jpg/png/gif format.
 
-The tool is based on project ['powerpoint'](https://github.com/pythonicrubyist/powerpoint). It cooperates well with [Smallpdf](https://smallpdf.com/cn/pdf-to-jpg), so that we can coordinate Latex with Powerpoint.
+The tool is based on project ['powerpoint'](https://github.com/pythonicrubyist/powerpoint). It cooperates well with [Smallpdf](https://smallpdf.com/cn/pdf-to-jpg), a downloaded zip file can be the parameter of the script, unzip will be done automatically.
+
+Require:
+  gem install powerpoint
+
+Usage:
+  ./img2ppt : generate ppt using all the images in current folder as backgrounds
+or
+  ./img2ppt xxx.zip : unzip the zip file, and generate ppt based on unziped images in current foler
 
 License: WTFPL
 
--- img2ppt.rb
+++ img2ppt.rb
@@ -1,14 +1,40 @@
+#!/usr/bin/ruby
 # Copyright @ Ye XIA <qazxiaye@126.com>
 
 require 'powerpoint'
 
-@deck = Powerpoint::Presentation.new
-
-coords = {x: 0, y: 0, cx: 9147791, cy: 6870610}
-
-for img in Dir["#{Dir.pwd}/*.jpg"]
-    @deck.add_pictorial_slide '', img, coords
+if ARGV.length > 1
+	puts "Usage:"
+	puts "\033\[0;32m  ./img2ppt\033\[0m : generate ppt using all the images in current folder as backgrounds"
+	puts "  or"
+	puts "\033\[0;32m  ./img2ppt xxx.zip\033\[0m : unzip the zip file, and generate ppt based on unziped images in current foler"
+	exit
 end
 
-@deck.save('test.pptx')
+if ARGV.length == 1
+	puts `unzip -o #{ARGV[0]}`
+end
+
+@deck = Powerpoint::Presentation.new
+coords = {x: 0, y: 0, cx: 9147791, cy: 6870610}
+
+isImg = false
+for img in Dir["#{Dir.pwd}/*.jpg"]
+    @deck.add_pictorial_slide '', img, coords
+	isImg = true
+end
+for img in Dir["#{Dir.pwd}/*.png"]
+    @deck.add_pictorial_slide '', img, coords
+	isImg = true
+end
+for img in Dir["#{Dir.pwd}/*.gif"]
+    @deck.add_pictorial_slide '', img, coords
+	isImg = true
+end
+
+if isImg
+	@deck.save('test.pptx')
+else
+	puts "\033\[0;33mNo image found!\033\[0m"
+end
 
